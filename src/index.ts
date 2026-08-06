d2642f16fea282b6a122e98a5a9a9117047bc7a6
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2026-08-06 16:37:47 +0530

    feat: add matchers with setup entry

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { detectServerKind, type ServerKind } from './detect.js'
 export { McpHarness, mcpTest } from './harness.js'
+export { mcpMatchers, registerMatchers } from './matchers.js'
 export type {
   McpServerInput,
   McpTestOptions,
